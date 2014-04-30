6db640992cc20caac0719137f19168ae2ada8527
nickwiecien <nickwiecien@gmail.com>
2014-04-30 14:21:33 -0500

    Added ChemicalFormulaModification

---
+++ NeuQuant.lib/ChemicalFormulaModification.cs
@@ -0,0 +1,28 @@
+﻿using CSMSL.Chemistry;
+using CSMSL.Proteomics;
+
+namespace NeuQuant
+{
+    public class ChemicalFormulaModification : Modification
+    {
+        public ChemicalFormula ChemicalFormula { get; private set; }
+        public bool IsAminoAcid { get; private set; }
+        public bool IsDefault { get; private set; }
+
+        public ChemicalFormulaModification(string chemicalFormula, string name, ModificationSites sites, bool isAminoAcid = false, bool isDefault = false)
+            : base(chemicalFormula, name, sites)
+        {
+            IsDefault = isDefault;
+            ChemicalFormula = new ChemicalFormula(chemicalFormula);
+            IsAminoAcid = isAminoAcid;
+        }
+
+        public ChemicalFormulaModification(ChemicalFormula chemicalFormula, string name, ModificationSites sites, bool isAminoAcid = false, bool isDefault = false)
+            : base(chemicalFormula.MonoisotopicMass, name, sites)
+        {
+            IsDefault = isDefault;
+            ChemicalFormula = chemicalFormula;
+            IsAminoAcid = isAminoAcid;
+        }
+    }
+}
